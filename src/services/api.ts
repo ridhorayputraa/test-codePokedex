af8bf316e2ee62710299ed10663c25e5f8d1b2c8
Ridho Ray <ridhorayputra033@gmail.com>
2023-09-02 21:29:00 +0700

    chore: intialize api, Pokemon endpoints and Types endpoints

---
+++ src/services/api.ts
@@ -0,0 +1,37 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+
+// Define a type for Pokemon
+export type Pokemon = {
+    id: number;
+    name: string;
+    // Add other fields relevant to Pokemon here
+  };
+
+
+export type Type = {
+    name: string;
+}; 
+
+export const pokemonApi = createApi({
+  reducerPath: 'pokemonApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+
+  endpoints: (builder) => ({
+
+    getPokemonByName: builder.query<Pokemon, string>({
+      query: (name) => `pokemon/${name}`,
+    }),
+
+    // Get By Types
+    getPokemonByTypes: builder.query<Type, string>({
+        query: (id) => `type/${id}`,
+    })
+
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPokemonByNameQuery, useGetPokemonByTypesQuery } = pokemonApi
