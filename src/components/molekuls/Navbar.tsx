94303424a9217696cdad07583e91a3e9fe134676
Ridho Ray <ridhorayputra033@gmail.com>
2023-09-04 09:17:59 +0700

    feat: layouting

--- src/components/molekuls/Navbar.tsx
+++ src/components/molekuls/Navbar.tsx
@@ -7,12 +7,15 @@ import { Link, BrowserRouter } from "react-router-dom";
 function Navbar() {
   return (
     
-    <div className='w-screen border border-red-400'>
+    <div className='w-screen max-w-screen-xl border flex flex-row justify-center  border-red-400'>
+      <div className=" max-w-screen-xl">
+
       <p>
         Pokedex
       </p>
   
         <Link to={`/pokemon`}>Favorite</Link>
+      </div>
     
     </div>
     
